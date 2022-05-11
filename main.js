95144b56b6957e3c923c8be7556b73682dc1598f
AdvayTike <94460020+AdvayTike@users.noreply.github.com>
2022-05-11 14:46:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function adduser() {
+
+    player1_name = document.getElementById("player_1");
+    player2_name = document.getElementById("player_2");
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+
+    window.location = "game_page.html"
+}
